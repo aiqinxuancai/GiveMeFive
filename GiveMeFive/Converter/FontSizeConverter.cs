7a203cad3c844c9c300e28e9f365a904df1e0d21
爱琴炫彩 <aiqinxuancai@163.com>
2017-12-13 16:26:55 +0800

    字体尺寸优化

--- GiveMeFive/Converter/FontSizeConverter.cs
+++ GiveMeFive/Converter/FontSizeConverter.cs
@@ -63,4 +63,32 @@ namespace GiveMeFive.Converter
 
         public int Cutoff { get; set; }
     }
+
+
+    /// <summary>
+    /// 用于动态转换左下角字体大小
+    /// </summary>
+    public class FontSizeConverterMini8 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double gridHeight = (double)value;
+                double fontSize = 6d / 240d * gridHeight;
+                return fontSize;
+            }
+            catch (System.Exception ex)
+            {
+                return 30;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Cutoff { get; set; }
+    }
 }
